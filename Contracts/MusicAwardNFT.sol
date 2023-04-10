f3a2f0a8ad054162379c65116ea01cb3f35d9bd5
ivxry <123484508+ivxry@users.noreply.github.com>
2023-04-10 10:20:04 +0200

    NatSpec Edit

--- Contracts/MusicAwardNFT.sol
+++ Contracts/MusicAwardNFT.sol
@@ -6,6 +6,9 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Strings.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
+/// @title NFT des récompenses musicales
+/// @author Marc Kouassi pour Alyra
+/// @notice Ce contrat permet de créer des NFT pour représenter des récompenses musicales
 contract MusicAwardNFT is ERC721URIStorage, Ownable {
     using Strings for uint256;
     using Counters for Counters.Counter;
@@ -13,6 +16,7 @@ contract MusicAwardNFT is ERC721URIStorage, Ownable {
     Counters.Counter private _tokenIdCounter;
     mapping (address => bool) private _approvedMinters;
 
+    /// @notice Structure des informations d'une récompense musicale
     struct Award {
         string artist;
         string songTitle;
@@ -22,28 +26,47 @@ contract MusicAwardNFT is ERC721URIStorage, Ownable {
         string awardType;
     }
 
+    /// @notice Mapping entre les tokens et les informations des récompenses musicales
     mapping (uint256 => Award) public awardInfo;
 
+    /// @notice Event émis lorsqu'une récompense est créée
     event AwardMinted(address indexed minter, uint256 indexed tokenId, string indexed awardType);
 
+    /// @notice Modifier pour autoriser uniquement les émetteurs approuvés
     modifier onlyApprovedMinter() {
         require(isApprovedMinter(msg.sender), "Only approved minters can mint awards");
         _;
     }
+
+    /// @notice Constructor du contrat
     constructor() ERC721("Music Award NFT", "MA") {}
 
+    /// @notice Ajoute un émetteur approuvé
+    /// @param minter L'adresse de l'émetteur à ajouter
     function addApprovedMinter(address minter) public onlyOwner {
         _approvedMinters[minter] = true;
     }
 
+    /// @notice Retire un émetteur approuvé
+    /// @param minter L'adresse de l'émetteur à retirer
     function removeApprovedMinter(address minter) public onlyOwner {
         _approvedMinters[minter] = false;
     }
 
+    /// @notice Vérifie si une adresse est un émetteur approuvé
+    /// @param minter L'adresse à vérifier
+    /// @return true si l'adresse est un émetteur approuvé, sinon @return false
     function isApprovedMinter(address minter) public view returns (bool) {
         return _approvedMinters[minter];
     }
 
+    /// @notice Crée un NFT de la récompense musicale
+    /// @param artist L'artiste ayant reçu la récompense
+    /// @param songTitle Le titre de la chanson
+    /// @param certificationDate La date de certification
+    /// @param recordLabel Le label musical
+    /// @param album L'album de la récompense
+    /// @param awardType Le type de récompense (Or, Platine, Diamant)
     function mintAward(
         string memory artist,
         string memory songTitle,
@@ -94,6 +117,9 @@ contract MusicAwardNFT is ERC721URIStorage, Ownable {
         emit AwardMinted(msg.sender, newTokenId, awardType);
     }
 
+/// @notice Transfère un NFT à un destinataire
+/// @param tokenId L'identifiant du token à transférer
+/// @param recipient L'adresse du destinataire
     function transferAward(uint256 tokenId, address recipient) public onlyOwner {
         require(_exists(tokenId), "Token does not exist");
         require(recipient != address(0), "Invalid recipient address");
